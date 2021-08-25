a7d90c9a1560c2f6c989c46168cd3d4b5375887c
DEWAN <dewanmahinchy@gmail.com>
2021-08-25 17:07:47 +0600

    search product added

--- dom.js
+++ dom.js
@@ -121,3 +121,48 @@ document.getElementById("add-item").addEventListener("click", function(){
 document.getElementById("item-container").addEventListener("click",function(event){
     event.target.parentNode.removeChild(event.target);
 })
+
+// ===================================================
+// Searching string as a product
+// ===================================================
+const products = [
+    'Dell hardcore i29 200GB laptop',
+    'iphone 1TB camera flashlight Phone',
+    'yellow laptop with black camera',
+    'Dell 1X59 Lenovo commercial yoga laptop',
+    'LG supernova laptop Dell',
+    'HTC low price Phone',
+    'Dell purple color phone with Laptop'
+]
+const searching = "dell";
+
+const output = [];
+
+// indexOf
+for(const product of products){
+    if(product.toLowerCase().indexOf(searching.toLowerCase()) != -1){
+        output.push(product);
+    }
+}
+
+// includes
+for(const product of products){
+    if(product.toLowerCase().includes(searching.toLowerCase())){
+        output.push(product);
+    }
+}
+
+// Starts with
+for(const product of products){
+    if(product.toLowerCase().startsWith(searching.toLowerCase())){
+        output.push(product);
+    };
+}
+
+// Ends with
+for(const product of products){
+    if(product.toLowerCase().endsWith(searching.toLowerCase())){
+        output.push(product);
+    }
+}
+console.log(output);
