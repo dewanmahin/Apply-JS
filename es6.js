629c982415f09e64ab1a64d07c53b686b1308f47
DEWAN <dewanmahinchy@gmail.com>
2023-02-19 22:12:41 +0600

    chaining & optinal chaining

--- es6.js
+++ es6.js
@@ -66,3 +66,31 @@ console.log(largest); */
 // const phone = fish.phone;
 const {phone, price} = fish;
 console.log(price); */
+
+//=============== Chaining & Optional Chaining ===============//
+const users = [ {id: 1, name: 'abul', job: 'doctor'} ]
+// console.log(users[0].name);
+
+const data = {
+    count: 5000,
+    data: [
+        {id: 1, name: 'babul', job: 'leader'},
+        {id: 2, name: 'dabul', job: 'leader'},
+    ]
+}
+// console.log(data.data[1].name);
+
+const user = {
+    id: 5001,
+    name: 'Thomas alba edison',
+    address: {
+        street: {
+            first: '35/A kochukhet lane',
+            second: 'third floor',
+            third: 'right side'
+        },
+        postOffice: 'cantonment',
+        city: 'Dhaka'
+    }
+} 
+console.log(user?.address?.stret)
