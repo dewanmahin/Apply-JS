45f71797f7dde65999170c1ef75fc5f8a0e5aac0
DEWAN <dewanmahinchy@gmail.com>
2023-02-20 18:42:41 +0600

    exploring filter and find

--- es6.js
+++ es6.js
@@ -152,3 +152,23 @@ products.forEach(product => {
     productsName.push(product.name)
 });
 console.log(productsName); */
+
+//=============== filter() and find() ===============//
+// filter //
+/* const numbers = [12, 5, 23, 45, 11, 18, 9, 55, 61, 1];
+const bigNums = numbers.filter(number => number > 20);
+// console.log(bigNums);
+
+const products = [
+    {id: 1, name: 'laptop', price: 45000},
+    {id: 1, name: 'mobile', price: 80000},
+    {id: 1, name: 'watch', price: 35000},
+    {id: 1, name: 'tablet', price: 23000},
+];
+const expensive = products.filter(product => product.price > 100000);
+// console.log(expensive);
+
+// find //
+const numbers1 = [12, 5, 23, 45, 11, 18, 9, 55, 61, 1];
+const five = numbers1.find(num => num % 5 == 0);
+// console.log(five); */
